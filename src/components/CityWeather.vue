0303882c5469357c5b525515af82bb905acd02f8
Andrey Vladykov <avladykov@outlook.com>
2020-10-10 13:24:46 +0300

    Add city weather component.

---
+++ src/components/CityWeather.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{city}}</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'CityWeather',
+  props: {
+    city: {
+      type: String,
+      required: true
+    }
+  },
+  setup () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
