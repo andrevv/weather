34f87c3855ced0a27162e657eb059657d64cf370
Andrey Vladykov <avladykov@outlook.com>
2020-10-30 19:21:17 +0300

    Add the OpenWeather API key search result.

--- api/src/main/java/dev/andrevv/weather/api/config/RestTemplateConfig.java
+++ api/src/main/java/dev/andrevv/weather/api/config/RestTemplateConfig.java
@@ -2,12 +2,14 @@ package dev.andrevv.weather.api.config;
 
 import dev.andrevv.weather.api.client.openweathermap.OpenWeatherClient;
 import dev.andrevv.weather.api.client.openweathermap.OpenWeatherClientImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@Slf4j
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
@@ -17,6 +19,7 @@ public class RestTemplateConfig {
     @Bean
     public OpenWeatherClient openWeatherClient(RestTemplate restTemplate) {
         String apiKey = System.getenv("API_KEY");
+        log.info("OpenWeather API key found: {}", apiKey.replaceAll("(?<=^.{3}).*", "*****"));
         return new OpenWeatherClientImpl(restTemplate, apiKey);
     }
 }
