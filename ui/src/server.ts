2a77f12697f7bb5f70d439682353263ada170c39
Andrey Vladykov <avladykov@outlook.com>
2020-11-02 19:44:55 +0300

    Add typescript support.

--- ui/src/server.ts
+++ ui/src/server.ts
@@ -3,10 +3,6 @@ import { createServer } from 'miragejs'
 export default function makeServer () {
   createServer({
     routes () {
-      this.get('/api/weather/cities', () => {
-
-      })
-
       this.get('/api/weather/cities/:id', (schema, request) => {
         const cities = {
           moscow: { celsius: 19, fahrenheit: 66 },
--- ui/src/views/City.vue
+++ ui/src/views/City.vue
@@ -9,7 +9,7 @@
 <script lang="ts">
 
 import { defineComponent } from 'vue'
-import CityTemperature from '@/components/CityTemperature'
+import CityTemperature from '@/components/CityTemperature.vue'
 
 export default defineComponent({
   name: 'Home',
