60ea4847eb0cc26e27180bbb0ca62f8d2d979a80
Andrey Vladykov <avladykov@outlook.com>
2020-11-08 23:46:12 +0300

    Add weather icon components.

---
+++ ui/src/components/CityWeatherIcon.vue
@@ -0,0 +1,32 @@
+<template>
+  <IconClear v-if="name === 'clear'"/>
+  <IconRain v-if="name === 'rain'"/>
+  <IconCloudy v-if="name === 'cloudy'"/>
+  <IconSnow v-if="name === 'snow'"/>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+import IconClear from '@/components/IconClear.vue'
+import IconRain from '@/components/IconRain.vue'
+import IconCloudy from '@/components/IconCloudy.vue'
+import IconSnow from '@/components/IconSnow.vue'
+
+export default defineComponent({
+  name: 'CityWeatherIcon',
+  components: {
+    IconClear,
+    IconRain,
+    IconCloudy,
+    IconSnow
+  },
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+})
+
+</script>
