d3756d9fe0ab0ee7d91ce30daacd976a3dbdcc2c
Andrey Vladykov <avladykov@outlook.com>
2020-11-29 16:48:21 +0300

    Add a module for cities API.

---
+++ api-cities/src/test/java/dev/andrevv/weatherapicities/ApiCitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andrevv.weatherapicities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
