e069a05d695f1ed11f7f807bf8e056a4ced07611
Andrey Vladykov <avladykov@outlook.com>
2020-12-01 00:41:32 +0300

    Add logging to the cities controller.

--- api-cities/src/main/java/dev/andrevv/weather/api/cities/controller/CityController.java
+++ api-cities/src/main/java/dev/andrevv/weather/api/cities/controller/CityController.java
@@ -2,12 +2,14 @@ package dev.andrevv.weather.api.cities.controller;
 
 import dev.andrevv.weather.api.cities.entity.City;
 import dev.andrevv.weather.api.cities.service.CitySearcher;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/cities")
+@Slf4j
 public class CityController {
 
     private final CitySearcher searcher;
@@ -18,6 +20,7 @@ public class CityController {
 
     @GetMapping("/search")
     public List<City> cities(@RequestParam("token") String token) {
+        log.info("Got a request for '{}'.", token);
         return searcher.search(token);
     }
 }
